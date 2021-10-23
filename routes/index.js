c6090a4ba62072ae30b0451a1284d8fa2b781228
Amatul Saboor Siddiqua <amatulsaboor221@gmail.com>
2021-10-23 18:36:23 +0500

    async await fixed ( 4 CRUD operations with 4 different wways)

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,12 @@ const uri = 'mongodb+srv://Admin:admin123@cluster0.vzs9g.mongodb.net/myDB?retryW
 const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true});
 
 /* GET home page. */
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
   client.connect(async (err) => {
     if (err) throw err;
     const usersCollection = client.db('myDB').collection('UsersCollection');
-    await usersCollection.find({}).toArray( (err, userData) => {
-      if (err) throw err;
-      res.render('./index.ejs', {userData});
-    });
+    const userData = await usersCollection.find({}).toArray();
+    res.render('./index.ejs', {userData});
   });
 });
 
@@ -21,44 +19,38 @@ router.post('/add', (req, res) => {
   client.connect(async (err) => {
     if (err) throw err;
     const usersCollection = client.db('myDB').collection('UsersCollection');
-    await usersCollection.insertOne(req.body, (err, result) => {
-      if (err) throw err;
-      if (result.acknowledged) {
-        console.log(result.insertedCount + ` document inserted successfully!`);
-      } else {
-        console.log(`There was an error in inserting the document`);
-      }
-      res.redirect('/');
-    });
+    // using async await
+    const result = await usersCollection.insertOne(req.body);
+    if (result.acknowledged) {
+      console.log(result.insertedCount + ` document inserted successfully!`);
+    } else {
+      console.log(`There was an error in inserting the document`);
+    }
+    res.redirect('/');
   });
 });
 
 // ============================== delete ======================================
 router.get('/delete/:userId', (req, res) => {
   console.log('inside delete');
-  client.connect(async (err) => {
+  client.connect((err) => {
     if (err) throw err;
     const usersCollection = client.db('myDB').collection('UsersCollection');
-    await usersCollection.deleteOne({'_id': new ObjectId(req.params.userId)}, (err, result) => {
-      if (err) throw err;
-      if (result.acknowledged) {
-        console.log(result.deletedCount + ' document deleted successfully!');
-      } else {
-        console.log('There was an error in deleting the document');
-      }
+    // using promise
+    usersCollection.deleteOne({'_id': new ObjectId(req.params.userId)}).then(result => console.log(result)).catch( err => console.log(err));
       res.redirect('/');
     });
   });
-});
 
 // ============================== update ======================================
 router.post('/edit', (req, res) =>{
   console.log('inside update');
   console.log(req.body);
-  client.connect(async (err) => {
+  client.connect(err => {
     if (err) throw err;
     const usersCollection = client.db('myDB').collection('UsersCollection');
-    await usersCollection.updateOne({'_id': new ObjectId(req.body._id)}, {$set: {'name':req.body.name, 'email':req.body.email, 'address':req.body.address, 'phone':req.body.phone}}, (err, result) => {
+    // using callback
+    usersCollection.updateOne({'_id': new ObjectId(req.body._id)}, {$set: {'name':req.body.name, 'email':req.body.email, 'address':req.body.address, 'phone':req.body.phone}}, (err, result) => {
       if (err) throw err;
       if (result.acknowledged) {
         console.log(result.updatedCount + ' document updated successfully!');
